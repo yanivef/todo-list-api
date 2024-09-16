a81c6e9a4306d4ad111758db6ac5facea3d90567
Yaniv Efroni <yan1v20005@gmail.com>
2024-09-16 17:57:16 +0300

    Initial commit

---
+++ models/task.go
@@ -0,0 +1,11 @@
+package main
+
+import "time"
+
+type Task struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Done      bool      `json:"done"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
